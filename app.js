db5e9fbe3c363b5ac7a95f649cea074aa6205068
Itachi_Uchiha <aviral1101@gmail.com>
2024-12-29 17:05:56 +0530

    connection-to-mongoDB

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express")
+const mongoose = require('mongoose')
 const env = require("dotenv")
 const app = express();
 
 env.config();
 
 const PORT = process.env.PORT 
-
+mongoose.set("strictQuery", true);
+try {
+   mongoose.connect(process.env.MONGO_URL, {
+    useUnifiedTopology : true   ,
+    useNewUrlParser: true
+   }) 
+   console.log("Connected to MongoDB");
+} catch (error) {
+    console.log(`Error in connecting to MongoDB with error : ${error}`);
+    process.exit();
+    
+}
 app.listen(PORT,(req,res) => console.log(`Listening on PORT ${PORT}`))
