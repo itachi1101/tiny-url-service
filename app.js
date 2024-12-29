bb8362efca33cfa9c1d0bb4eefc7e637f2838ba3
Itachi_Uchiha <aviral1101@gmail.com>
2024-12-29 16:52:47 +0530

    initial-setup

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// my very first service
+const express = require("express")
+const env = require("dotenv")
+const app = express();
+
+env.config();
+
+const PORT = process.env.PORT 
+
+app.listen(PORT,(req,res) => console.log(`Listening on PORT ${PORT}`))
